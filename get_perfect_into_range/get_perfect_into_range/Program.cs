c210b11239156372403beedb9e2867ebbb37308b
MohamedAboSeada <120672803+MohamedAboSeada@users.noreply.github.com>
2022-12-16 23:17:42 +0200

    Add files via upload

---
+++ get_perfect_into_range/get_perfect_into_range/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// [1] take the factors of number 
+class Program {
+    static void Main() {
+        Console.WriteLine("--------------------------");
+        Console.WriteLine("| Get Perfect into range |");
+        Console.WriteLine("--------------------------\n");
+        Console.Write("Enter start point : ");
+        int n1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter end point : ");
+        
+        int n2 = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n2; i++) {
+            int sum = 0;
+            for (int j = 1; j < i; j++) {
+                if (i%j == 0) {
+                    sum += j;
+                }
+            }
+            if (sum == i) {
+                Console.WriteLine("'{0}' is Perfect",i);
+            }
+        }
+        Console.ReadLine();
+    }
+}
